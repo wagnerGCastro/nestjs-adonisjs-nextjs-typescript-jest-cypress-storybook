4406e9c91b06d666bb2b6ccad235b6a33dfa9d0a
Wagner Castro <wagner_gcastro@hotmail.com>
2022-02-16 02:23:43 -0300

    #04 front(setup-jest) - Configuration jest ann test page home

--- frontend/src/contexts/settings.context.tsx
+++ frontend/src/contexts/settings.context.tsx
@@ -1,4 +1,5 @@
 import React, { useMemo } from "react";
+
 export interface State {
   settings: any;
 }
@@ -18,17 +19,14 @@ export const SettingsContext = React.createContext<State | any>(initialState);
 
 SettingsContext.displayName = "SettingsContext";
 
-export const SettingsProvider: React.FC<{ initialValue: any }> = ({
-  initialValue,
-  ...props
-}) => {
+export const SettingsProvider: React.FC<{ initialValue: any }> = ({ initialValue, ...props }) => {
   const [state, updateSettings] = React.useState(initialValue ?? initialState);
   const value = useMemo(
     () => ({
       ...state,
       updateSettings,
     }),
-    [state]
+    [state],
   );
   return <SettingsContext.Provider value={value} {...props} />;
 };
