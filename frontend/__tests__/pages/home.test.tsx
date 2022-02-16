2f97a89cf9cd01336044b0e54cbe06f306ab04ae
Wagner Castro <wagner_gcastro@hotmail.com>
2022-02-16 08:23:42 -0300

    #05 front(storybook) - Add Storybook and configuration in project

--- frontend/__tests__/pages/home.test.tsx
+++ frontend/__tests__/pages/home.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { mount } from "enzyme";
-import Home from "../../src/pages/home";
+import Home from "@/pages/home";
 
 describe("Pages", () => {
   describe("Home", () => {
